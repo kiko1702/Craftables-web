a0b869a3ee041253d044919d54c7dd97ad03aa53
kiko1702 <162837367+kiko1702@users.noreply.github.com>
2024-03-16 22:36:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.pro-container');
+hiddenElements.forEach((el) => observer.observe(el));
