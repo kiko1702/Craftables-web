79052e9cdfed294a7b2e66bc171c62e6540c1a60
kiko1702 <162837367+kiko1702@users.noreply.github.com>
2024-05-18 17:40:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,17 +14,40 @@ if (close) {
     })
 }
 
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        }
-        else {
-            entry.target.classList.remove('show');
         }
     });
 });
 
 const hiddenElements = document.querySelectorAll('.pro-container');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+function singleproductpage(){
+    const producttp = 1;
+}
+
+function sendMail() {
+    var params = {
+        Name : document.getElementById("conname").value,
+        Email : document.getElementById("conemail").value,
+        Message : document.getElementById("conmessage").value
+    }
+    emailjs.send("service_jceqj07", "template_oifsoq2", params)
+}
+
+function singleproductpage() {
+    var clickedChild = document.querySelector('#karimdiv .pro-container:hover');
+    if (clickedChild) {
+        console.log('Clicked child:', clickedChild.textContent);
+    }
+    const productimage = clickedChild.firstElementChild.firstElementChild.href
+
+    document.getElementById("singlediv").innerHTML="<img>";
+}
