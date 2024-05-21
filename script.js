316b23ce33ae96cdb62ce0a3f996871c0e7bc692
kiko1702 <162837367+kiko1702@users.noreply.github.com>
2024-05-21 18:52:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
+const mobile = document.getElementById('mobile');
 const close = document.getElementById('close');
+const products = [
+    {
+        id: 0,
+        image: 'img/products-imgs/CraftAbles ring fake -m- (2).JPG',
+        title: 'CraftAbles ring',
+        price: 30,
+    },
+    {
+        id: 1,
+        image: 'img/products-imgs/cbc blue bracelet.png',
+        title: 'CbC blue bracelet',
+        price: 25,
+    },
+    {
+        id: 2,
+        image: 'img/products-imgs/cbc green bracelet.png',
+        title: 'CbC green bracelet',
+        price: 25,
+    },
+    {
+        id: 3,
+        image: 'img/products-imgs/cbc white bracelet.png',
+        title: 'CbC white bracelet',
+        price: 25,
+    },
+    {
+        id: 4,
+        image: 'img/products-imgs/white rocky bracelet.png',
+        title: 'White rocky bracelet',
+        price: 35,
+    }
+]
 
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
+        mobile.classList.add('hide');
     })
 }
 
 if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
+        mobile.classList.remove('hide');
     })
 }
 
@@ -39,7 +74,22 @@ function sendMail() {
         Email : document.getElementById("conemail").value,
         Message : document.getElementById("conmessage").value
     }
-    emailjs.send("service_jceqj07", "template_oifsoq2", params)
+    emailjs.send("service_jceqj07", "template_oifsoq2", params);
+    window.location.href = './Verification';
+
+}
+
+function Sellform(){
+    var dataparams = {
+        SellerName : document.getElementById("selname").value,
+        SellerEmail : document.getElementById("selemail").value,
+        SellerProductName : document.getElementById("selproname").value,
+        SellerProductImage : document.getElementById("selproimg").value,
+        SellerProductPrice : document.getElementById("selproprice").value,
+        SellerProductDescription : document.getElementById("selmessage").value
+    }
+    emailjs.send("service_ffy298b", "template_odfgtvh", dataparams);
+    window.location.href = './Verification'
 }
 
 function singleproductpage() {
@@ -51,3 +101,56 @@ function singleproductpage() {
 
     document.getElementById("singlediv").innerHTML="<img>";
 }
+
+const ringcra = document.getElementById('ringcra');
+
+ringcra.addEventListener('click', function() {
+  window.location.href = './Product-127283';
+});
+
+const blubracelet = document.getElementById('blubracelet');
+
+blubracelet.addEventListener('click', function() {
+  window.location.href = './Product-246824';
+});
+
+const greenbracelet = document.getElementById('greenbracelet');
+
+greenbracelet.addEventListener('click', function() {
+  window.location.href = './Product-246824';
+});
+
+const whitebracelet = document.getElementById('whitebracelet');
+
+whitebracelet.addEventListener('click', function() {
+  window.location.href = './Product-652170';
+});
+
+const rockybracelet = document.getElementById('rockybracelet');
+
+rockybracelet.addEventListener('click', function() {
+    window.location.href = './Product-987521';
+  });
+
+  var cart = [];
+  const cartdiv = document.getElementById("cart-status");
+  function displaycart(a){
+    let j = 0;
+    if(cart.length==0){
+        console.log(j);
+    }
+    else{
+        document.getElementById("cartstatus").innerHTML = cart.map((items)=> {
+            var{image, title, price} = items;
+            if(cart.length = 0){} 
+            else{
+                cartdiv.innerHTML = cart.map((items) => {
+                    var {image, title, price} = items;
+                    return(
+                        '<div class="cart-item"><div class="row-img"><img class="rowimg" src=${image}></div>>p style="font-size: 12px;">${title}</p><h2>${price}</h2><div onclick="delel("+(j++) +");" <p>Remove</p>'
+                    );
+                })
+            }
+        })
+    }
+  }
