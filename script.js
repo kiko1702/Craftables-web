23b997adf9474cbe2e74ddd2dcc4fd46c0a6c48c
kiko1702 <162837367+kiko1702@users.noreply.github.com>
2024-05-24 16:26:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,38 +2,6 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const mobile = document.getElementById('mobile');
 const close = document.getElementById('close');
-const products = [
-    {
-        id: 0,
-        image: 'img/products-imgs/CraftAbles ring fake -m- (2).JPG',
-        title: 'CraftAbles ring',
-        price: 30,
-    },
-    {
-        id: 1,
-        image: 'img/products-imgs/cbc blue bracelet.png',
-        title: 'CbC blue bracelet',
-        price: 25,
-    },
-    {
-        id: 2,
-        image: 'img/products-imgs/cbc green bracelet.png',
-        title: 'CbC green bracelet',
-        price: 25,
-    },
-    {
-        id: 3,
-        image: 'img/products-imgs/cbc white bracelet.png',
-        title: 'CbC white bracelet',
-        price: 25,
-    },
-    {
-        id: 4,
-        image: 'img/products-imgs/white rocky bracelet.png',
-        title: 'White rocky bracelet',
-        price: 35,
-    }
-]
 
 if (bar) {
     bar.addEventListener('click', () => {
@@ -74,7 +42,7 @@ function sendMail() {
         Email : document.getElementById("conemail").value,
         Message : document.getElementById("conmessage").value
     }
-    emailjs.send("service_jceqj07", "template_oifsoq2", params);
+    emailjs.send("service_ffy298b", "template_oifsoq2", params);
     window.location.href = './Verification';
 
 }
@@ -117,40 +85,17 @@ blubracelet.addEventListener('click', function() {
 const greenbracelet = document.getElementById('greenbracelet');
 
 greenbracelet.addEventListener('click', function() {
-  window.location.href = './Product-246824';
+  window.location.href = './Product-987521';
 });
 
 const whitebracelet = document.getElementById('whitebracelet');
 
 whitebracelet.addEventListener('click', function() {
-  window.location.href = './Product-652170';
+  window.location.href = './Product-987521 ';
 });
 
 const rockybracelet = document.getElementById('rockybracelet');
 
 rockybracelet.addEventListener('click', function() {
-    window.location.href = './Product-987521';
+    window.location.href = './Product-652170';
   });
-
-  var cart = [];
-  const cartdiv = document.getElementById("cart-status");
-  function displaycart(a){
-    let j = 0;
-    if(cart.length==0){
-        console.log(j);
-    }
-    else{
-        document.getElementById("cartstatus").innerHTML = cart.map((items)=> {
-            var{image, title, price} = items;
-            if(cart.length = 0){} 
-            else{
-                cartdiv.innerHTML = cart.map((items) => {
-                    var {image, title, price} = items;
-                    return(
-                        '<div class="cart-item"><div class="row-img"><img class="rowimg" src=${image}></div>>p style="font-size: 12px;">${title}</p><h2>${price}</h2><div onclick="delel("+(j++) +");" <p>Remove</p>'
-                    );
-                })
-            }
-        })
-    }
-  }
