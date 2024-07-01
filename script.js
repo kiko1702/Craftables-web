c3224b7b90fe10ba19bc135b5c11ecc6d9fdcd3a
kiko1702 <162837367+kiko1702@users.noreply.github.com>
2024-07-01 14:56:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -60,33 +60,51 @@ function singleproductpage() {
     document.getElementById("singlediv").innerHTML="<img>";
 }
 
+var product = 0;
+
 const ringcra = document.getElementById('ringcra');
 
 ringcra.addEventListener('click', function() {
   window.location.href = './Product-127283';
+    product = ringcra;
 });
 
 const blubracelet = document.getElementById('blubracelet');
 
 blubracelet.addEventListener('click', function() {
   window.location.href = './Product-246824';
+  product = blubracelet;
 });
 
 const greenbracelet = document.getElementById('greenbracelet');
 
 greenbracelet.addEventListener('click', function() {
-  window.location.href = './Product-987521'; 
+  window.location.href = './Product-987521';
+  product = greenbracelet; 
 });
 
 const whitebracelet = document.getElementById('whitebracelet');
 
 whitebracelet.addEventListener('click', function() {
   window.location.href = './Product-789350';
-});
+  product = whitebracelet;
+}); 
 
 const rockybracelet = document.getElementById('rockybracelet');
 
 rockybracelet.addEventListener('click', function() {
     window.location.href = './Product-652170';
+    product = rockybracelet;
   });
 
+const divcart = document.getElementById('cart-status');
+function Addtocart() {
+    localStorage.setItem("cart", product)
+    window.location.href = "cart.html"
+    const item = localStorage.getItem("cart")
+    divcart.innerHTML += '<div class="cartitem"><div class="productty"> <div class="itdes"><p>'+item+'</p></div></div></div>'
+}
+
+const test = "test"
+localStorage.setItem("testconst", test);
+localStorage.getItem("testconst")
